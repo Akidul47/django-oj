f2be899f3f89f9ad2c8884d6dc52ac1eb2c0419a
by-sabbir <me@sabbir.dev>
2022-10-30 14:06:51 +0600

    refactor: basic form control

--- oj/urls.py
+++ oj/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SubmitView
 
 
 urlpatterns = [
-    path("", IndexView.as_view()),
+    path('', IndexView.as_view()),
+    path('thanks/', SubmitView.as_view()),
 ]
--- oj/views.py
+++ oj/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from .forms import CompilerModelForm
 
-class IndexView(FormView):
+class IndexView(CreateView):
     template_name = "oj/index.html"
     form_class = CompilerModelForm
     success_url = '/thanks/'
 
+    def form_valid(self, form):
+        print("form validation: ", self.get_context_data())
+        print("is valid: ", form.is_valid())
+        return super().form_valid(form)
 
+
+class SubmitView(TemplateView):
+    template_name = "oj/success.html"
